03716ec43e56f9c5939b94f7cb58b13bee3a8d19
Thomas Coats <github@voodoolabs.net>
2019-08-21 09:24:15 +1200

    code comments

--- index.js
+++ index.js
@@ -2,15 +2,19 @@ const classIdSplit = /([\.#]?[a-zA-Z0-9\u007F-\uFFFF_:-]+)/
 const notClassId = /^\.|#/
 
 module.exports = (h) => (element, data, children) => {
+  // null means div
   if (element == null)
     return h('div', data, children)
+  // support h(Component) syntax
   if (typeof(element) != 'string')
     return h(element, data, children)
+  // support h('div', [ ]) syntax (optional params)
   if (data == null || typeof(data) != 'object' || Array.isArray(data)) {
     children = data
     data = {}
   }
 
+  // parse parts and assign to classes and id
   const parts = element.split(classIdSplit)
   for (let part of parts) {
     const type = part.charAt(0)
